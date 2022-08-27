7423ed18862ab08cc27cc31d2eff7bf1c1924bd8
seankeish111 <seankeish@Seans-MBP.modem>
2022-08-27 18:12:29 +1000

    Successfully created the cloud and UI login

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "intelbismoveee94bbf05": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "analytics": {
+        "intelbismovee": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    },
+    "function": {
+        "intelbismovee73ebb391": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "ilabsmovee": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
