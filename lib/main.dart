b1ba247ff1feabd06fa4e03d86d6d2b737ca9572
seankeish111 <seankeish@Seans-MBP.modem>
2022-08-28 18:29:56 +1000

    hello world with the amplify cloud

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,85 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_datastore/amplify_datastore_stream_controller.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:movee_aws/screens/authGate.dart';
-
 import 'amplifyconfiguration.dart';
+import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
+import 'package:amplify_core/amplify_core.dart';
+import 'package:movee_aws/models/ModelProvider.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:provider/provider.dart';
+
+
+
+
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
+
 class _MyAppState extends State<MyApp> {
+
+  final _amplify = Amplify;
+  final _FormInputsID = "test";
+
   @override
   void initState() {
     super.initState();
     _configureAmplify();
   }
 
-  void _configureAmplify() async {
-    try {
-      await Amplify.addPlugin(AmplifyAuthCognito());
-      await Amplify.configure(amplifyconfig);
-      print('Successfully configured');
-    } on Exception catch (e) {
-      print('Error configuring Amplify: $e');
-    }
+  void _configureAmplify() {
+    final provider = ModelProvider();
+    final dataStorePlugin = AmplifyDataStore(modelProvider: provider);
+
+    // _amplify.addPlugin(dataStorePlugins: [dataStorePlugin]);
+    _amplify.configure(amplifyconfig);
+
+    print('Amplify configured');
+
   }
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    return authGate();
+    return MaterialApp(
+
+      title: 'Amplify crud',
+      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: Text('hey')
+    );
   }
+
+  void create() async {
+    final FormInputs = formInputs(
+      id: _FormInputsID, value: "test");
+
+    try {
+      await Amplify.DataStore.save(FormInputs);
+
+      print ('save ${FormInputs.toString()}');
+
+    } catch(e) {
+    } print(e);
+  }
+
+  formInputs({required String id, required String value}) {}
+
 }
+
+
+
+
+
--- lib/screens/authGate.dart
+++ lib/screens/authGate.dart
@@ -1,68 +1,68 @@
-import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:amplify_api/amplify_api.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:safeprint/safeprint.dart';
-
-
-
-import '../amplifyconfiguration.dart';
-import '../models/ModelProvider.dart';
-import 'package:movee_aws/main.dart';
-
-
-
-class authGate extends StatefulWidget {
-  @override
-  _authGateState createState() => _authGateState();
-}
-
-class _authGateState extends State<authGate> {
-  @override
-  void initState() {
-    super.initState();
-    _configureAmplify();
-  }
-
-  Future<void> _configureAmplify() async {
-    final api = AmplifyAPI(modelProvider: ModelProvider.instance);
-    await Amplify.addPlugin(api);
-
-    try {
-      await Amplify.configure(amplifyconfig);
-    } on AmplifyAlreadyConfiguredException {
-      print(
-          'Tried to reconfigure Amplify; this can occur when your app restarts on Android.');
-    }
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-
-
-  Future<void> createFormInputs() async {
-    try {
-      final formInputs = FormInputs(first_name: 'my first todo', second_name: 'test' );
-      final request = ModelMutations.create(String);
-      final response = await Amplify.API.mutate(request: request).response;
-
-      final createdFormInputs = response.data;
-      if (createdFormInputs == null) {
-        print('errors: ${response.errors}');
-        return;
-      }
-      print('Mutation result: ${createdFormInputs.first_name}');
-    } on ApiException catch (e) {
-      print('Mutation failed: $e');
-    }
-  }
-
-
-}
-
-
-
-
+// import 'package:amplify_flutter/amplify_flutter.dart';
+// import 'package:amplify_api/amplify_api.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:safeprint/safeprint.dart';
+//
+//
+//
+// import '../amplifyconfiguration.dart';
+// import '../models/ModelProvider.dart';
+// import 'package:movee_aws/main.dart';
+//
+//
+//
+// class authGate extends StatefulWidget {
+//   @override
+//   _authGateState createState() => _authGateState();
+// }
+//
+// class _authGateState extends State<authGate> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     _configureAmplify();
+//   }
+//
+//   Future<void> _configureAmplify() async {
+//     final api = AmplifyAPI(modelProvider: ModelProvider.instance);
+//     await Amplify.addPlugin(api);
+//
+//     try {
+//       await Amplify.configure(amplifyconfig);
+//     } on AmplifyAlreadyConfiguredException {
+//       print(
+//           'Tried to reconfigure Amplify; this can occur when your app restarts on Android.');
+//     }
+//   }
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     throw UnimplementedError();
+//   }
+//
+//
+//   Future<void> createFormInputs() async {
+//     try {
+//       final formInputs = FormInputs(first_name: 'my first todo', second_name: 'test' );
+//       final request = ModelMutations.create(String);
+//       final response = await Amplify.API.mutate(request: request).response;
+//
+//       final createdFormInputs = response.data;
+//       if (createdFormInputs == null) {
+//         print('errors: ${response.errors}');
+//         return;
+//       }
+//       print('Mutation result: ${createdFormInputs.first_name}');
+//     } on ApiException catch (e) {
+//       print('Mutation failed: $e');
+//     }
+//   }
+//
+//
+// }
+//
+//
+//
+//
